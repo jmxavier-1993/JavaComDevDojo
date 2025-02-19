b4958f66341370149634a6921da13b7f04acabeb
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2025-02-19 14:00:54 -0300

    java com devdojo

---
+++ src/ExercicioImparOuPar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ExercicioImparOuPar {
+    public static void main(String[] args) {
+        int numero=0;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um número");
+        numero= ler.nextInt();
+
+        if (numero%2==0){
+            System.out.println("é par");
+        } else {
+            System.out.println("é impar");
+        }
+    }
+}
---
+++ src/ViradoNoJiraya.java
@@ -0,0 +1,10 @@
+public class ViradoNoJiraya {
+    public static void main(String[] args) {
+        int idade=35;
+        System.out.println("Hi guys minha idade é"+" "+ idade);
+        //comentario in line
+        /*
+        multiplas linhas
+         */
+    }
+}
