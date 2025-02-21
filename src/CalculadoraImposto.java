975b7b78686c01b80c966febaf8ad4505f1aca06
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2025-02-21 15:28:04 -0300

    Aulas lógica devdojo

---
+++ src/CalculadoraImposto.java
@@ -0,0 +1,8 @@
+public class CalculadoraImposto {
+    public static void main(String[] args) {
+        float salario= (float) 2500f;
+        double percentSalario= 5;
+        double valorPercentSalario=(percentSalario/100) * salario;
+        System.out.println(valorPercentSalario);
+    }
+}
---
+++ src/ReutilizaVariavel.java
@@ -0,0 +1,16 @@
+public class ReutilizaVariavel {
+    public static void main(String[] args) {
+        double salario=2500;
+        double percentSalario= 30;
+        double valorPercentSalario=(percentSalario/100) * salario;
+        System.out.println("30% eh:" +valorPercentSalario);
+
+        percentSalario=15;
+        valorPercentSalario=(percentSalario/100) * salario;
+        System.out.println("15% eh:" +valorPercentSalario);
+
+        percentSalario=5;
+        valorPercentSalario=(percentSalario/100) * salario;
+        System.out.println("5% eh:" +valorPercentSalario);
+    }
+}
