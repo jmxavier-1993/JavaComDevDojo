9375a179bd635a1bbe4c04831076ab4f71401f14
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2025-03-19 00:31:03 -0300

    novos arquivos

---
+++ src/academy/devdojo/lacosderepeticao/fors/Aula01Introducaofors.java
@@ -0,0 +1,10 @@
+package academy.devdojo.lacosderepeticao.fors;
+
+public class Aula01Introducaofors {
+    public static void main(String[] args) {
+        //cria a variavel, insere o ponto de parada do for e coloca o iterador pra ir acrescentando.Por prática usa-se I
+        for (int contador=0;contador<=10;contador++)
+        {
+            System.out.println(contador);
+    }
+}}
