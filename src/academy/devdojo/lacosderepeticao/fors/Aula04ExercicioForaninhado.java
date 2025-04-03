6449e84aca60e4d5c998fd58c589fd0d7412aa84
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2025-04-02 21:47:55 -0300

    nova aula

---
+++ src/academy/devdojo/lacosderepeticao/fors/Aula04ExercicioForaninhado.java
@@ -0,0 +1,11 @@
+package academy.devdojo.lacosderepeticao.fors;
+
+public class Aula04ExercicioForaninhado {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <=10 ; j++) {
+                System.out.println("A Tabuada de "+i + "x"+j +"eh:"+(i*j) );
+            }
+        }
+    }
+}
